e4272ae8faaf2225bd83d46d9c7b304e8e9584ac
brtk-k <bartoszk@stanford.edu>
2024-03-11 20:16:46 -0700

    Plotting updates

--- src/plotting/plotting.jl
+++ src/plotting/plotting.jl
@@ -3,3 +3,5 @@ function plotReachabilityCloud end
 function plotReachabilityCloudRGBSlice end
 function plotReachabilityCloudSlice end
 function plotFilamentCollapsedRings! end
+function plotConfigurationsSelfWeight! end
+function plotConfigurationTubesSelfWeight! end
